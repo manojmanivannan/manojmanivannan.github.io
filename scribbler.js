6e2bd36e6f1002597ce931fb31760312cefd915b
MANOJ MANIVANNAN <manojm18@live.in>
2021-08-24 01:14:35 +0530

    BikeSharing update regression

--- scribbler.js
+++ scribbler.js
@@ -159,3 +159,10 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+$(document).keydown(function(event) { 
+  if (event.keyCode == 27) { 
+    $('#myModal').hide();
+  }
+});
+
